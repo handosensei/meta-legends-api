2a2f32fbb8c0e283be859fc900dfcd8dd86b51d1
Hando Masahashi <handosensei@gmail.com>
2025-06-14 09:29:09 +0200

    create token

--- src/metadata/service/token.service.ts
+++ src/metadata/service/token.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Token } from '../entity/token.entity';
+import { Collection } from '../entity/collection.entity';
 
 @Injectable()
-export class TokenService {}
+export class TokenService {
+  private static readonly logger = new Logger(TokenService.name);
+
+  constructor(
+    @InjectRepository(Token)
+    private tokenRepository: Repository<Token>,
+  ) {}
+
+  buildToken(collection: Collection, metadata: any, file: string) {
+    const token = new Token();
+    token.collection = collection;
+    token.number = this.defineTokenNumber(file);
+    if ('name' in metadata) {
+      token.name = metadata['name'];
+    }
+    if ('image_url' in metadata) {
+      token.imageUrl = metadata['image_url'];
+    }
+    if ('animation_url' in metadata) {
+      token.animationUrl = metadata['animation_url'];
+    }
+    return token;
+  }
+
+  defineTokenNumber(file: string) {
+    // console.log(file);
+    // const fileNameWithoutExtension = file.split('.').slice(0, -1).join('.');
+    // console.log('fileNameWithoutExtension');
+    // console.log(fileNameWithoutExtension);
+    return Number(file);
+  }
+}
